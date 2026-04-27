50168788889f1bccc485f9608f0e2fa56a836d6d
Ankit Kumar <heyankitkr@gmail.com>
2026-04-28 05:14:45 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy_day = 0
+        sell_day = 1
+        max_profit = 0
+        while sell_day < len(prices):
+            if prices[buy_day] < prices[sell_day]:
+                profit = prices[sell_day] - prices[buy_day]
+                max_profit = max(max_profit,profit)
+            else:
+                buy_day = sell_day
+
+            sell_day += 1
+        return max_profit
